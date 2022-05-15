0a26eedc0ea922e9cc23791ba51ab4fca036de2c
zhang-mingyuan123 <2369558390@qq.com>
2022-05-16 01:25:24 +0800

    123

--- canton-house/src/index.tsx
+++ canton-house/src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '@/pages/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <App />
 );
