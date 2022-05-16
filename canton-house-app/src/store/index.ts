ac4287fb712ff29c2c948d9a073f333b3d11044e
zhang-mingyuan123 <2369558390@qq.com>
2022-05-16 12:33:13 +0800

    123

--- canton-house-app/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import NumStore from './modules/num-store'
-
-export const stores = React.createContext({
-  num: NumStore
-})
-
-export const useStores = () => React.useContext(stores)
