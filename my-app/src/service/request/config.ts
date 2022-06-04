d1b838b925f6c0c95cb99d8553bcc2124088327b
zhang-mingyuan123 <2369558390@qq.com>
2022-06-04 11:47:19 +0800

    Ming 完善项目地址

--- my-app/src/service/request/config.ts
+++ my-app/src/service/request/config.ts
@@ -2,11 +2,11 @@
  * @Author: Ming
  * @Date: 2022-05-16 12:20:55
  * @LastEditors: Ming
- * @LastEditTime: 2022-06-02 17:20:37
+ * @LastEditTime: 2022-06-04 11:46:30
  * @Description: 请填写简介
  */
 
 const TIME_OUT = 5000
-const BASE_URL = 'http://180.184.74.142'
+const BASE_URL = 'http://180.184.74.142:30001/'
 
 export { BASE_URL, TIME_OUT }
